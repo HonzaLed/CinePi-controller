ac2e7237045f7ace62032427265666a7df1b75f0
HonzaLed <honzaled@proton.me>
2023-10-10 21:53:38 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let hostname;
+</script>
+
+<div class="max-h-screen flex flex-col items-center justify-center bg-gray-200">
+    <div class="container mx-auto flex justify-center items-center">
+        <!-- Your centered image -->
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/16x9_by_Pengo.svg/1920px-16x9_by_Pengo.svg.png" alt="Centered Image" class="">
+        
+        <!-- Side controls -->
+        <div class="ml-8 flex flex-col">
+            <button class="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 rounded-lg mb-4">Control 1</button>
+            <button class="bg-green-500 text-white hover:bg-green-600 px-4 py-2 rounded-lg">Control 2</button>
+        </div>
+    </div>
+</div>
